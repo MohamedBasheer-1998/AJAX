29348493ca09ca44f7e0c05b429db59148a6057a
Mohamed Basheer <basheer@buckman.com>
2024-08-03 13:37:53 +0530

    Add project files.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using AJAX.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AJAX.Controllers
+{
+    public class ItemController : Controller
+    {
+        private static List<Item> items = new List<Item>();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(items);
+        }
+
+        [HttpPost]
+        public JsonResult AddItem(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return Json(new { success = false, message = "Item name cannot be empty." });
+            }
+
+            var newItem = new Item { Id = items.Count + 1, Name = name };
+            items.Add(newItem);
+
+            return Json(new { success = true, item = newItem });
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace AJAX.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
